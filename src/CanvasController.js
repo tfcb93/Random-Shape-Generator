d7a90ff2f9ec14f5859d7f6dbc7b7bfa0b1f1057
Thiago Borges <thiagoborges533@gmail.com>
2023-01-30 11:39:58 -0300

    Changes on the color generation using chroma JS

--- src/CanvasController.js
+++ src/CanvasController.js
@@ -1,4 +1,5 @@
-import Points from './Points';
+import Points from "./Points";
+import chroma from "chroma-js";
 
 /**
  * Canvas controller
@@ -6,72 +7,85 @@ import Points from './Points';
  */
 
 class Canvas {
-    constructor(canvasWidth, canvasHeight, quads) {
-        // The line bellow can be change to create the canvas element and insert it on the document.body;
-        this.canvas = document.getElementById('canvas');
-        this.ctx = this.canvas.getContext('2d');
+  constructor(canvasWidth, canvasHeight, quads) {
+    // The line bellow can be change to create the canvas element and insert it on the document.body;
+    this.canvas = document.getElementById("canvas");
+    this.ctx = this.canvas.getContext("2d");
 
-        this.canvas.width = canvasWidth;
-        this.canvas.height = canvasHeight;
-        this.quads = quads;
+    this.canvas.width = canvasWidth;
+    this.canvas.height = canvasHeight;
+    this.quads = quads;
 
-        this.shapes = [];
+    this.shapes = [];
 
-        this.canvas.addEventListener('click', () => {
-            this.addShape();
-        });
-    }
+    this.canvas.addEventListener("click", () => {
+      this.addShape();
+    });
+  }
 
-    // Drawing points on the canvas
-    drawPoints() {
-        const color = this.changeColorRandomly();
-        this.ctx.strokeStyle = color;
-        this.ctx.beginPath();
-        this.shapes.forEach((points) => {
-            points.drawPoints(this.ctx);
-            points.decreaseLifeTime(1);
-        });
-        this.ctx.stroke();
-    }
+  // Drawing points on the canvas
+  drawPoints() {
+    this.ctx.beginPath();
+    this.shapes.forEach((points) => {
+      points.drawPoints(this.ctx);
+      points.decreaseLifeTime(1);
+    });
+    // this.ctx.stroke();
+    this.ctx.resetTransform();
+  }
 
-    // Responsible to randomly change the point values
-    changePoints() {
-        this.shapes.forEach((points) => {
-            points.distortAllPoints(
-                Math.floor(Math.random() * (6 - (-5)) + (-5)),
-                Math.floor(Math.random() * (6 - (-5)) + (-5))
-            );
-        });
-    }
+  // Responsible to randomly change the point values
+  changePoints() {
+    this.shapes.forEach((points) => {
+      //   points.distortAllPoints(
+      //     Math.floor(Math.random() * (6 - -5) + -5),
+      //     Math.floor(Math.random() * (6 - -5) + -5)
+      //   );
+      //   points.distortPoints(
+      //     Math.floor(Math.random() * (2 - -1) + -1),
+      //     Math.floor(Math.random() * (2 - -1) + -1)
+      //   );
+      points.distortPoints();
+    });
+  }
 
-    // Responsible to randomly change the color
-    changeColorRandomly() {
-        const newColor = [
-            Math.floor(Math.random() * 255),
-            Math.floor(Math.random() * 255),
-            Math.floor(Math.random() * 255),
-            Math.random(),
-        ];
+  createColorArray() {
+    const scale = chroma
+      .scale([chroma.random(), chroma.random()])
+      .mode("hcl")
+      .colors(100);
 
-        return `rgba(${newColor.join(", ")})`;
-    }
+    console.log(scale);
 
-    changeWindowSize(newWidth, newHeight) {
-        this.canvas.width = newWidth;
-        this.canvas.height = newHeight;
-    }
+    return scale;
+  }
 
+  // Responsible to randomly change the color
+  changeColorRandomly() {
+    const newColor = [
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.floor(Math.random() * 255),
+      Math.random(),
+    ];
 
-    // add new shapes for the shapes array
-    addShape() {
-        this.shapes.push(new Points(this.quads));
-    }
+    return `rgba(${newColor.join(", ")})`;
+  }
 
-    // delete dead shapes from the shapes array
-    buryDeadShapes() {
-        this.shapes = this.shapes.filter((points) => points.isDead === false);
-    }
+  changeWindowSize(newWidth, newHeight) {
+    this.canvas.width = newWidth;
+    this.canvas.height = newHeight;
+  }
 
+  // add new shapes for the shapes array
+  addShape() {
+    this.shapes.push(new Points(this.quads));
+  }
+
+  // delete dead shapes from the shapes array
+  buryDeadShapes() {
+    this.shapes = this.shapes.filter((points) => points.isDead === false);
+  }
 }
 
 export default Canvas;
--- src/Points.js
+++ src/Points.js
@@ -1,60 +1,85 @@
-import Point from './Point';
+import Point from "./Point";
+import chroma from "chroma-js";
 
 /**
  * Points class that keep a collection of 4 points
  */
 
 class Points {
-    constructor(quads) {
-        this.points = [];
-        this.generateFourPoints(quads);
-        this.lifeTime = Math.floor(Math.random() * (3600 - 900) + 900);
-        this.isDead = false;
-    }
+  constructor(quads) {
+    this.points = [];
+    this.generateFourPoints(quads);
+    this.color = chroma.random();
+    this.colorDeg = chroma(this.color).get("hsl.h");
+    this.direction = this.defineDirection();
+    this.lifeTime = Math.floor(Math.random() * (3600 - 900) + 900);
+    this.isDead = false;
+  }
 
-    generateFourPoints(quads) {
-        for(let i = 1; i <= 4; i++) {
-            this.points.push(
-                new Point(
-                    Math.floor(Math.random() * (quads[i].x.start - quads[i].x.end) + quads[i].x.end),
-                    Math.floor(Math.random() * (quads[i].y.start - quads[i].y.end) + quads[i].y.end)
-                )
-            );
-        }
+  generateFourPoints(quads) {
+    for (let i = 1; i <= 4; i++) {
+      this.points.push(
+        new Point(
+          Math.floor(
+            Math.random() * (quads[i].x.start - quads[i].x.end) + quads[i].x.end
+          ),
+          Math.floor(
+            Math.random() * (quads[i].y.start - quads[i].y.end) + quads[i].y.end
+          )
+        )
+      );
     }
+  }
 
-    // draw each points of its collection on canvas
-    drawPoints(ctx) {
-        this.points.forEach((point, index) => {
-            if(index === 0) {
-                ctx.moveTo(point.x, point.y);
-            } else {
-                ctx.lineTo(point.x, point.y);
-            }
-        });
-        ctx.closePath();
+  defineDirection() {
+    return [
+      Math.floor(Math.random() * (2 - -1) + -1),
+      Math.floor(Math.random() * (2 - -1) + -1),
+    ];
+  }
+
+  // draw each points of its collection on canvas
+  drawPoints(ctx) {
+    ctx.save();
+    this.changeColor();
+    ctx.strokeStyle = this.color;
+    this.points.forEach((point, index) => {
+      if (index === 0) {
+        ctx.moveTo(point.x, point.y);
+      } else {
+        ctx.lineTo(point.x, point.y);
+      }
+    });
+    ctx.closePath();
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  changeColor() {
+    this.colorDeg += 5;
+    if (this.colorDeg > 360) this.colorDeg = 0;
+    this.color = chroma(this.color).set("hsl.h", this.colorDeg);
+  }
+
+  // "distort" points method, so it changes the x and y of each point in its collection
+  distortPoints(addX = 1, addY = 1) {
+    this.points.forEach((point) => {
+      point.distortInXandY(this.direction[0] + addX, this.direction[1] + addY);
+    });
+  }
+
+  // decrease a value of its lifetime
+  decreaseLifeTime(val) {
+    this.lifeTime -= val;
+    this.checkIfItsDead();
+  }
+
+  // check if this colleciton of points (form) is dead
+  checkIfItsDead() {
+    if (this.lifeTime <= 0) {
+      this.isDead = true;
     }
-
-
-    // "distort" points method, so it changes the x and y of each point in its collection
-    distortAllPoints(addX, addY) {
-        this.points.forEach((point) => {
-            point.distortInXandY(addX, addY);
-        });
-    }
-
-    // decrease a value of its lifetime
-    decreaseLifeTime(val) {
-        this.lifeTime -= val;
-        this.checkIfItsDead();
-    }
-
-    // check if this colleciton of points (form) is dead
-    checkIfItsDead() {
-        if(this.lifeTime <= 0) {
-            this.isDead = true;
-        }
-    }
+  }
 }
 
 export default Points;
